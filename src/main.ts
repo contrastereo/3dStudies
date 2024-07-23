4372b2361c3d17ccc1fc708e53190d698d7c26e1
Luis Pirela <lpirela90@gmail.com>
2024-07-23 01:01:21 -0300

    three js lesson 1

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,31 @@
-import * as THREE from 'three';
-import { BlasterScene } from './BlasterScene';
+import * as Three from 'three';
 
-const width = window.innerWidth;
-const height = window.innerHeight;
+//initial size
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
 
-const renderer = new THREE.WebGLRenderer({
-	canvas: document.querySelector('#app') as HTMLCanvasElement,
+//Scene
+const scene = new Three.Scene();
+
+//Red Cube
+const geometry = new Three.BoxGeometry(1, 1, 1);
+const material = new Three.MeshBasicMaterial({ color: 'red' });
+const mesh = new Three.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = { width: 800, height: 600 };
+
+//Camera
+const camera = new Three.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 0.5;
+camera.position.y = 0.2;
+scene.add(camera);
+
+// Renderer
+const renderer = new Three.WebGLRenderer({
+	canvas,
 });
 
-renderer.setSize(width, height);
+renderer.setSize(sizes.width, sizes.height);
 
-const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100);
-
-const scene = new BlasterScene(mainCamera);
-
-scene.initialize();
-
-function tick() {
-	scene.update();
-	renderer.render(scene, mainCamera);
-	requestAnimationFrame(tick);
-}
-
-tick();
+renderer.render(scene, camera);
